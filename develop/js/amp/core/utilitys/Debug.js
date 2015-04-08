35fb01bc8aa080cf1f1a4858a782f3fa6b91549e
fujiwara <fujiwara@invogue.co.jp>
2015-04-08 23:39:21 +0900

    Debug.js追加

--- develop/js/amp/core/utilitys/Debug.js
+++ develop/js/amp/core/utilitys/Debug.js
@@ -7,14 +7,14 @@
   ----------------------------------------------------------------------*/
 
   /**
-   * <h4>ƒfƒoƒbƒNƒƒO‚ğo—Í</h4>
+   * <h4>ãƒ‡ãƒãƒƒã‚°æ©Ÿèƒ½</h4>
    *
    * @class Log
    * @constructor
    */
   function Debug(){}
 
-  // Šî’êƒNƒ‰ƒX‚ğŒp³
+  // åŸºåº•ã‚¯ãƒ©ã‚¹ã‚’ç¶™æ‰¿
   AMP.inherits(Debug, AMP._AMP);
 
   // prototype
@@ -25,8 +25,9 @@
   /*--------------------------------------------------------------------------
     @property
   --------------------------------------------------------------------------*/
+
   /**
-   * <h4>ƒo[ƒWƒ‡ƒ“î•ñ</h4>
+   * <h4>ãƒãƒ¼ã‚¸ãƒ§ãƒ³æƒ…å ±</h4>
    *
    * @static
    * @property VERSION
@@ -36,7 +37,7 @@
 
 
   /**
-   * <h4>ƒNƒ‰ƒX–¼</h4>
+   * <h4>ã‚¯ãƒ©ã‚¹å</h4>
    *
    * @private
    * @property name
@@ -46,12 +47,12 @@
 
 
   /**
-   * <h4>ƒƒO‚ğ•\¦‚·‚éview</h4>
+   * <h4>ãƒ‡ãƒãƒƒã‚°view</h4>
    *
-   * @property view
-   * @type {DOM}
+   * @property css
+   * @type {Object}
    */
-  Debug.view = null;
+  p._debugViews = null;
 
 
 
@@ -62,23 +63,112 @@
 
   // Log.hsaLog = !(root.console._defaultLog);
   Debug.createView = function(){
+    // view?p?[?c?vâ€˜f?d?Â¶?ï¿¢
+    var childNode = '';
+    childNode += '<div style="z-index:19791218;min-width:250px;font-size:12px;background:#41454e;">';
+    childNode += '<div class="ttl" style="padding:5px;line-height:12px;font-weight:bold;color:#f9f9f9;text-align:center;background:#272a32;">DEBUG</div>';
+    childNode += '<textarea id="AMP_DEBUG_TEXT" style="-webkit-box-sizing:border-box;box-sizing:border-box;width:100%;min-height:150px;padding:10px;font-family:consolas;color:#272a32;font-size:14px;line-height:1.5;border:5px solid #41454e;"></textarea>';
+    childNode += '</div>';
+
+    // view?vâ€˜f?d?Â¶?ï¿¢
+    var doc = document,
+    el = doc.createElement('div');
+
+    el.id = 'AMP_DEBUG';
+    el.innerHTML = childNode;
+    doc.body.appendChild(el);
+
+    //?@controll elms
+    Debug.views = {
+      wrap : doc.getElementById('AMP_DEBUG'),
+      text : doc.getElementById('AMP_DEBUG_TEXT')
+    };
+
+    Debug.views.wrap.setAttribute('style', 'position:fixed;left:10px;bottom:10px;');
+
+    //
+    Debug.addEvent();
   };
 
-  Debug.moveView = function(){
+
+  Debug.addEvent = function(){
+   // move
+    var isDrag = false,
+    x = null,
+    y = null;
+
+    // down
+    Debug.views.wrap.onmousedown  = function(){
+      isDrag = true;
+    };
+
+    // cancel
+    Debug.views.text.onmousemove = function(){
+      isDrag = false;
+    };
+
+    // move
+    Debug.views.wrap.onmousemove = function(){
+      if(isDrag){
+        var _x = event.clientX;
+        _y = event.clientY;
+
+        if(AMP.isNumber(x)){
+          var diffX = _x - x;
+          var diffY = _y - y;
+          x = _x;
+          y = _y;
+
+          var offset = 'position:fixed;';
+          offset += 'top:' + (Debug.views.wrap.offsetTop + diffY) + 'px;';
+          offset += 'left:' + (Debug.views.wrap.offsetLeft + diffX) + 'px;';
+          Debug.views.wrap.setAttribute('style', offset);
+
+        } else {
+          x = _x;
+          y = _y;
+        }
+
+        return false;
+      }
+    };
+
+    // up
+    document.onmouseup = function(){
+      isDrag = false;
+      x = null;
+      y = null;
+    };
   };
 
 
-  Debug.dataAnalysis = function(data){
+
+
+
+  /**
+   * <h4>ãƒ­ã‚°ã‚’å‡ºåŠ›ã—ã¾ã™</h4>
+   *
+   * @method log
+   * @return {Debug}
+   */
+  p.log = function(){
+    if(!this.views){
+      Debug.createView();
+    }
+
+    // ãƒ‡ãƒ¼ã‚¿ã‚¿ã‚¤ãƒ—ã«åˆã‚ã›ã¦ãƒ­ã‚°ã‚’å‡ºåŠ›
+    AMP.each(AMP.argsToArray(arguments), function(data){
+      if(AMP.isArray(data)){
+        this.views.text.value += JSON.stringify(data) + '\n';
+      } else if(AMP.isObject(data)){
+        this.views.text.value += JSON.stringify(data, null, '\t') + '\n';
+      } else {
+        this.views.text.value += data + '\n';
+      }
+    });
+
+    return this;
   };
-
-  Debug.addLog = function(){
-  };
-
-  Debug.clearLog = function(){
-  };
-
-
-
 
 
 
