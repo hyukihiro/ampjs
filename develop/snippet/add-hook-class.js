11c0c13d1f5139d37a7385cdfef2385134a76650
yoshihitofujiwara <mpc.sound@gmail.com>
2015-07-07 10:12:43 +0900

    snippet/add-hook-class.js update

--- develop/snippet/add-hook-class.js
+++ develop/snippet/add-hook-class.js
@@ -1,31 +1,38 @@
 (function(){
 	/**
 	 * add hook class
-	 * version: 1.1
-	 *
-	 * html要素にフックとなるクラスを割り当てます。
-	 * head内に記述しcssより先に読み込ませます。
-	 *
-	 * namelist 以下は参考です。参考のクラス全て割り当てないこと。必要最小限のクラスを割り当てること。
-	 * osType: ['os-win', 'os-mac', 'os-ios', 'os-android']
-	 * deviceType: ['device-pc', 'device-sd', 'device-tb', 'device-sp']
-	 * browserType: ['browser-ie', 'browser-old-ie', 'browser-chrome', 'browser-firefox', 'browser-safari', 'browser-mobile-safari', 'browser-android']
+	 * version: 1.2.0
 	 */
 	var ua, html, addClass, preClass;
 
 	ua = navigator.userAgent.toLowerCase();
 	html = document.getElementsByTagName('html')[0];
 
-	// ここで必要なクラスの割り振りをします。
-	// 以下のように処理内容（クラス割り振った理由）はコメントで残してください。
+	// デバイスクラス追加
+	if(ua.match(/iphone|ipod|android/)){
+		addClass = 'device_smartdevice';
 
-	// デバイスは	、PCとSPのみで切り分け(タブレットはpc判定)
-	addClass = ua.match(/iphone|ipod|android/) && ua.indexOf('mobile') > -1 ? 'device-sp' : 'device-pc';
-	// 古いIE（8以下）のハック用
-	addClass += ua.indexOf('msie 8') > -1 ? ' browser-old-ie' : '';
+		if(ua.indexOf('mobile') > -1){
+			addClass += 'device_sp';
+		} else {
+			addClass += 'device_tablet';
+		}
+	} else {
+		addClass = 'device_pc';
+	}
+
+	// ブラウザクラス追加
+	if(ua.indexOf('msie 8') > -1){
+		addClass += ' browser-ie8';
+	} else if(ua.indexOf('msie 9') > -1){
+		addClass += ' browser_ie9';
+	} else {
+		addClass += ' browser_modern';
+	}
 
 	// addClass
 	preClass = html.getAttribute('class');
+
 	if(preClass){
 		addClass = preClass + ' ' + addClass;
 	}
